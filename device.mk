791d69ce984fa3ec93b05bbc39118127fe7ccd14
Apavayan <info@apavayan.com>
2016-04-15 09:36:58 +0530

    j7elte: Add Missing camera permission
    Change-Id: I0c05f6b833b6c3af5f600c113de730ba371fa47c
    Signed-off-by: Apavayan <info@apavayan.com>

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ TARGET_SCREEN_WIDTH := 720
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
