691757c9875743ffab79402e2f894ef5ef2d393a
Apavayan <info@apavayan.com>
2016-04-19 07:34:55 +0530

    j7elte: Use Custom audio effects
    Change-Id: Icfc4eff8d5924abdae2e7f041e88b0090e56f527

--- device.mk
+++ device.mk
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
