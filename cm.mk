39a14078266282ffc9cfd41a225bce31c8dc354e
Apavayan <info@apavayan.com>
2016-04-27 10:02:52 +0530

    j7elte: Overide device specific Stuff
    Change-Id: Id2cc5ca7cf019e70bcb7abe6cf5e085fdf68b956

--- cm.mk
+++ cm.mk
@@ -14,14 +14,14 @@
 # limitations under the License.
 #
 
+# Inherit from j7eltexx device
+$(call inherit-product, device/samsung/j7eltexx/device.mk)
+
 # Inherit common CM phone.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from j7eltexx device
-$(call inherit-product, device/samsung/j7eltexx/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := cm_j7eltexx
