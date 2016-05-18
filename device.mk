572e4c84a5532871101ad568cd4fb3b650bd44fd
Apavayan <info@apavayan.com>
2016-05-18 19:56:35 +0530

    j7elte: move media codecs to common
    Change-Id: I2147370b8992a05ea3f727c6171f2d1b8f6cb675
    Signed-off-by: Apavayan <info@apavayan.com>

--- device.mk
+++ device.mk
@@ -53,7 +53,6 @@ PRODUCT_PACKAGES += \
 
 # Media profile
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
 
 # Power
