6ac526e93a2a090dd5e8a752f397ae347ca99377
Apavayan Sinha <info@apavayan.com>
2016-07-24 21:17:08 +0530

    Updated Wifi Config
    Change-Id: I24ebe4ce2f2f016d66395df5a7c3aedb51237d00

--- device.mk
+++ device.mk
@@ -71,6 +71,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-fi
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/cred.conf:system/etc/wifi/cred.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
