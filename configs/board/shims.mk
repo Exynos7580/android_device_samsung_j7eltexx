17b5f5e38812cad6af79226ee1a2f8743a98b69d
messi2050 <minoforever216@gmail.com>
2018-09-08 17:46:36 +0200

    shims: add missing shim

--- configs/board/shims.mk
+++ configs/board/shims.mk
@@ -21,5 +21,6 @@
 
 TARGET_LD_SHIM_LIBS += \
     /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
-    /system/lib/hw/camera.vendor.universal7580.so|/vendor/lib/libshim_camera.so \
-    /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
+    /system/lib/hw/camera.universal7580.so|/vendor/lib/libshim_camera.so \
+    /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
+    /system/lib/libexynoscamera.so|/vendor/lib/libshim_camera.so
